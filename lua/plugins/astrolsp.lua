cafbfbe4a7e32c5972f5ed39a173dc96861ad67e
Jano Detzel <jano.detzel@gmail.com>
2025-03-20 04:26:43 +0100

    Setup Typescript

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -34,9 +32,12 @@ return {
         -- "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+      filter = function(client) -- fully override the default formatting function
+        -- disable when formatting via :EslintFixAll is possible
+        if client.name == "typescript-tools" and vim.fn.exists ":EslintFixAll" then return false end
+
+        return true
+      end,
     },
     -- enable servers that you already have installed without mason
     servers = {
