d8339d21cf5d74c34263bdc558cd634dca3df097
Jano Detzel <jano.detzel@gmail.com>
2025-03-20 04:26:39 +0100

    Add winresizer and mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -35,6 +35,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        winresizer_start_key = "<C-w>r",
       },
     },
     -- Mappings can be configured through AstroCore as well.
