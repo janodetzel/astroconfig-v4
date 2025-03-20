74fc2c4190ba150653c7bd5dc5ec71f050a99935
Jano Detzel <jano.detzel@gmail.com>
2025-03-20 04:26:32 +0100

    Add swift format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -17,6 +17,7 @@ return {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.swiftformat,
       null_ls.builtins.code_actions.refactoring,
 
       require "none-ls.diagnostics.eslint",
